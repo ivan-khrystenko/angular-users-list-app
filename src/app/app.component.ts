e06de5aebbebe29a1c7b858b54ebbec1acd2f09d
Ivan Khrystenko <hristenko.i@gmail.com>
2020-08-24 14:09:04 +0300

    My HW lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'users-list';
+  typesOfShoes: string[] = [
+    'Stalker',
+    'Boots',
+    'Clogs',
+    'Loafers',
+    'Moccasins',
+    'Sneakers',
+  ];
 }
