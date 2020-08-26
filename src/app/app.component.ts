724de5686a53e5f27c3e8c4c39b18f8c7bdd2be3
Ivan Khrystenko <hristenko.i@gmail.com>
2020-08-26 15:23:14 +0300

    My Lesson 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'users-list';
+  title = 'Users list';
   typesOfShoes: string[] = [
     'Stalker',
     'Boots',
